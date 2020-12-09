c8117db2229ddcae8fa0a9a860f6b16ae1a5c8c7
Hồ Quang Hải <hoquanghai.bka@gmail.com>
2020-12-09 14:20:47 +0900

    add image, comment RecyclerView

--- app/src/main/java/com/memo/MemoAdapter.kt
+++ app/src/main/java/com/memo/MemoAdapter.kt
@@ -41,63 +41,63 @@ class MemoAdapter(private val list: ArrayList<Memo>): RecyclerView.Adapter<MemoA
         var list_image: RecyclerView = itemView.findViewById(R.id.list_image)
         var list_comment: RecyclerView = itemView.findViewById(R.id.list_comment)
 
-        fun bind(memoResponse: Memo){
-            with(itemView) {
-                val createPerson = "${memoResponse.createPerson}"
-                val createdAt = "${memoResponse.createdAt}"
-                val memoTitle = "${memoResponse.memoTitle}"
-                val attachments = memoResponse.attachments
-                val url_avatar ="http://172.16.0.210/dr/dist/img/member/" +"${memoResponse.avatar}"
-
-                user_name.text = createPerson;
-                memo_title.text = memoTitle;
-                create_time.text = formatDate(createdAt)
-                Picasso.get().load(url_avatar)
-                        .into(user_avatar);
-
-                val childLayoutManager = LinearLayoutManager(MemoViewHolder.list_image.context, LinearLayout.HORIZONTAL, false)
-                holder.list_image.apply {
-                layoutManager = childLayoutManager
-                adapter = movie.attachments?.let { ImageAdapter(it) }
-                setRecycledViewPool(viewPool)
-        };
-
-
-
-                // comment view
-                val intent = (context as Activity).intent
-                val user = UserHelper(context).get()
-
-                Log.v("newMemo", gson.toJson(user))
-                val avatar_url = intent.getStringExtra("avatar_url")
-                if (avatar_url != null) {
-                    val comment_avatar = findViewById<CircleImageView>(R.id.comment_avatar)
-                    Picasso.get().load(avatar_url)
-                        .into(comment_avatar);
-                }
-
-                val comment_put = findViewById<ImageButton>(R.id.comment_put)
-                val editText1 = findViewById<EditText>(R.id.editText1)
-                comment_put.setOnClickListener{
-                     val newMemo = Memo(
-                             avatar = user?.picture?.thumbnail,
-                             createdAt = Date().toString(),
-                             createPerson = user?.name,
-                             group = "abc",
-                             userNumber = "njfng",
-                             memoTitle = editText1.getText().toString(),
-                             personId = "nnj",
-                             regular = "fhfh",
-                             position = "fgfg",
-                             updatedAt = "fgfg",
-
-                     )
-                    Log.v("newMemo", gson.toJson(newMemo))
-
-
-                }
-            }
-        }
+//        fun bind(memoResponse: Memo){
+//            with(itemView) {
+//                val createPerson = "${memoResponse.createPerson}"
+//                val createdAt = "${memoResponse.createdAt}"
+//                val memoTitle = "${memoResponse.memoTitle}"
+//                val attachments = memoResponse.attachments
+//                val url_avatar ="http://172.16.0.210/dr/dist/img/member/" +"${memoResponse.avatar}"
+//
+//                user_name.text = createPerson;
+//                memo_title.text = memoTitle;
+//                create_time.text = formatDate(createdAt)
+//                Picasso.get().load(url_avatar)
+//                        .into(user_avatar);
+//
+//                val childLayoutManager = LinearLayoutManager(MemoViewHolder.list_image.context, LinearLayout.HORIZONTAL, false)
+//                holder.list_image.apply {
+//                layoutManager = childLayoutManager
+//                adapter = movie.attachments?.let { ImageAdapter(it) }
+//                setRecycledViewPool(viewPool)
+//        };
+//
+//
+//
+//                // comment view
+//                val intent = (context as Activity).intent
+//                val user = UserHelper(context).get()
+//
+//                Log.v("newMemo", gson.toJson(user))
+//                val avatar_url = intent.getStringExtra("avatar_url")
+//                if (avatar_url != null) {
+//                    val comment_avatar = findViewById<CircleImageView>(R.id.comment_avatar)
+//                    Picasso.get().load(avatar_url)
+//                        .into(comment_avatar);
+//                }
+//
+//                val comment_put = findViewById<ImageButton>(R.id.comment_put)
+//                val editText1 = findViewById<EditText>(R.id.editText1)
+//                comment_put.setOnClickListener{
+//                     val newMemo = Memo(
+//                             avatar = user?.picture?.thumbnail,
+//                             createdAt = Date().toString(),
+//                             createPerson = user?.name,
+//                             group = "abc",
+//                             userNumber = "njfng",
+//                             memoTitle = editText1.getText().toString(),
+//                             personId = "nnj",
+//                             regular = "fhfh",
+//                             position = "fgfg",
+//                             updatedAt = "fgfg",
+//
+//                     )
+//                    Log.v("newMemo", gson.toJson(newMemo))
+//
+//
+//                }
+//            }
+//        }
 
 
     }
@@ -115,28 +115,28 @@ class MemoAdapter(private val list: ArrayList<Memo>): RecyclerView.Adapter<MemoA
 
     @SuppressLint("WrongConstant")
     override fun onBindViewHolder(holder: MemoViewHolder, position: Int) {
-        holder.bind(list[position])
-//        val movie = list[position]
-//        holder.create_time.text = formatDate(movie.createdAt)
-//        holder.memo_title.text = movie.memoTitle
-//        holder.user_name.text = movie.createPerson
-//        val url_avatar ="http://172.16.0.210/dr/dist/img/member/" +"${movie.avatar}"
-//        Picasso.get().load(url_avatar)
-//          .into(holder.user_avatar);
-//
-//        val commentLayoutManaget = LinearLayoutManager(holder.list_comment.context)
-//        holder.list_comment.apply {
-//            layoutManager = commentLayoutManaget
-//            adapter = movie.comments?.let { CommentAdapter(it) }
-//            setRecycledViewPool(viewPool2)
-//        }
-//
-//        val childLayoutManager = LinearLayoutManager(holder.list_image.context, LinearLayout.HORIZONTAL, false)
-//        holder.list_image.apply {
-//            layoutManager = childLayoutManager
-//            adapter = movie.attachments?.let { ImageAdapter(it) }
-//            setRecycledViewPool(viewPool)
-//        };
+//        holder.bind(list[position])
+        val movie = list[position]
+        holder.create_time.text = formatDate(movie.createdAt)
+        holder.memo_title.text = movie.memoTitle
+        holder.user_name.text = movie.createPerson
+        val url_avatar ="http://172.16.0.210/dr/dist/img/member/" +"${movie.avatar}"
+        Picasso.get().load(url_avatar)
+          .into(holder.user_avatar);
+
+        val commentLayoutManaget = LinearLayoutManager(holder.list_comment.context)
+        holder.list_comment.apply {
+            layoutManager = commentLayoutManaget
+            adapter = movie.comments?.let { CommentAdapter(it) }
+            setRecycledViewPool(viewPool2)
+        }
+
+        val childLayoutManager = LinearLayoutManager(holder.list_image.context, LinearLayout.HORIZONTAL, false)
+        holder.list_image.apply {
+            layoutManager = childLayoutManager
+            adapter = movie.attachments?.let { ImageAdapter(it) }
+            setRecycledViewPool(viewPool)
+        };
 
 
     }
